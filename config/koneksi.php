c8bfed1f098ef5ee9f5165d275118662685d38f0
Misayuuki <rafidahhusnia@gmail.com>
2024-12-12 20:42:28 +0700

    koneksi

---
+++ config/koneksi.php
@@ -0,0 +1,17 @@
+<?php 
+$host = "localhost";
+$username = "root";
+$password = "";
+$dbname = "db_absensi";
+
+try {
+    //koneksi dengan pdo
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    //set mode error pdo untuk tampilan exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // echo "Koneksi Mu Berhasil Nich!";
+} catch (PDOException $e) {
+    //menangkap error jika koneksi gagal
+    echo "Koneksi gagal:" . $e->getMessage();
+}
+?>
